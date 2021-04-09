016a08f69fea911bb4294eda570469e8a3fc8ae2
Peter Brand <peter-brand@xs4all.nl>
2021-04-09 21:26:37 +0200

    Add autopilot buttons

--- FSUIPC/A2A Cessna 172R Honeycomb Bravo/A2AC172RBravo.lua
+++ FSUIPC/A2A Cessna 172R Honeycomb Bravo/A2AC172RBravo.lua
@@ -7,15 +7,43 @@
 -- Pete & John Dowson @http://www.fsuipc.com/
 --
 
+local p3dControlIds = {
+      Vor1ObiDec    = 65662
+    , Vor1ObiInc    = 65663
+    , HeadingBugInc = 65879
+    , HeadingBugDec = 65880
+}
+
 local offsets = {
       AircraftName            = 0x3D00
     , AutoPilotMasterSwitch   = 0x07BC
-    , AutoPilotNav1Lock       = 0x07C4
-    , AutoPilotHeadingLock    = 0x07C8
-    , AutoPilotAltitudeLock   = 0x07D0
-    , AutoPilotApproachHold   = 0x0800
-    , AutoPilotBackCourseHold = 0x0804
 }
+local buttons = {
+      AutoPilotHdg       = 0
+    , AutoPilotNav       = 1
+    , AutoPilotApr       = 2
+    , AutoPilotRev       = 3
+    , AutoPilotAlt       = 4
+    , AutoPilotVs        = 5
+    , AutoPilotIas       = 6
+    , AutoPilotMaster    = 7
+    , AutoPilotIncrease  = 12
+    , AutoPilotDecrease  = 13
+    , AutoPilotSelectIas = 16
+    , AutoPilotSelectCrs = 17
+    , AutoPilotSelectHdg = 18
+    , AutoPilotSelectVs  = 19
+    , AutoPilotSelectAlt = 20
+}
+local apModeSelect = {
+      None = 0
+    , Ias = 1
+    , Crs = 2
+    , Hdg = 3
+    , Vs  = 4
+    , Alt = 5
+}
+local currentApModeSelect = apModeSelect.None
 local honeycombBravoDevice = {
       Vendor  = 0x294B -- Honeycomb Bravo vendor id
     , Product = 0x1901 -- Honeycomb Bravo product id
@@ -124,26 +152,6 @@ function AutoPilotMasterSwitchEvent(offset, value)
     currentA2aEventsState.IsApMasterEnabled = value > 0
 end
 
-function AutoPilotNav1LockEvent(offset, value)
-    currentA2aEventsState.IsApNavEnabled = value > 0
-end
-
-function AutoPilotHeadingLockEvent(offset, value)
-    currentA2aEventsState.IsApHeadingEnabled = value > 0
-end
-
-function AutoPilotAltitudeLockEvent(offset, value)
-    currentA2aEventsState.IsApAltEnabled = value > 0
-end
-
-function AutoPilotApproachHoldEvent(offset, value)
-    currentA2aEventsState.IsApAprEnabled = value > 0
-end
-
-function AutoPilotBackCourseHoldEvent(offset, value)
-    currentA2aEventsState.IsApRevEnabled = value > 0
-end
-
 function LowOilPressureLightEvent(varname, value, userParameter)
     currentA2aEventsState.IsOnLowOilPressureLight = value > 0
 end
@@ -204,6 +212,142 @@ function Eng1ChtEvent(varname, value, userParameter)
     currentA2aEventsState.Eng1Cht = value
 end
 
+function AutoPilotHdgButtonEvent(joynum, button, downup)
+    if downup == 1 then -- Pressed
+        ipc.writeLvar("kap140_hdg_button", 1)
+        ipc.writeLvar("kap140_hdg", 1)
+    elseif downup == 2 then -- Released
+        ipc.writeLvar("kap140_hdg_button", 0)
+    end
+end
+
+function AutoPilotNavButtonEvent(joynum, button, downup)
+    if downup == 1 then -- Pressed
+        ipc.writeLvar("kap140_nav_button", 1)
+        ipc.writeLvar("kap140_nav", 1)
+    elseif downup == 2 then -- Released
+        ipc.writeLvar("kap140_nav_button", 0)
+    end
+end
+
+function AutoPilotAprButtonEvent(joynum, button, downup)
+    if downup == 1 then -- Pressed
+        ipc.writeLvar("kap140_apr_button", 1)
+        ipc.writeLvar("kap140_apr", 1)
+    elseif downup == 2 then -- Released
+        ipc.writeLvar("kap140_apr_button", 0)
+    end
+end
+
+function AutoPilotRevButtonEvent(joynum, button, downup)
+    if downup == 1 then -- Pressed
+        ipc.writeLvar("kap140_rev_button", 1)
+        ipc.writeLvar("kap140_rev", 1)
+    elseif downup == 2 then -- Released
+        ipc.writeLvar("kap140_rev_button", 0)
+    end
+end
+
+function AutoPilotAltButtonEvent(joynum, button, downup)
+    if downup == 1 then -- Pressed
+        ipc.writeLvar("kap140_alt_button", 1)
+        ipc.writeLvar("kap140_alt", 1)
+    elseif downup == 2 then -- Released
+        ipc.writeLvar("kap140_alt_button", 0)
+    end
+end
+
+function AutoPilotVsButtonEvent(joynum, button, downup)
+    if downup == 1 then -- Pressed
+        ipc.writeLvar("kap140_arm_button", 1)
+        ipc.writeLvar("kap140_arm", 1)
+    elseif downup == 2 then -- Released
+        ipc.writeLvar("kap140_arm_button", 0)
+    end
+end
+
+function AutoPilotIasButtonEvent(joynum, button, downup)
+    if downup == 1 then -- Pressed
+        ipc.writeLvar("kap140_baro_button", 1)
+        ipc.writeLvar("kap140_baro", 1)
+    elseif downup == 2 then -- Released
+        ipc.writeLvar("kap140_baro_button", 0)
+    end
+end
+
+function AutoPilotMasterButtonEvent(joynum, button, downup)
+    if downup == 1 then -- Pressed
+        ipc.writeLvar("kap140_ap_button", 1)
+        ipc.writeLvar("kap140_ap", 1)
+    elseif downup == 2 then -- Released
+        ipc.writeLvar("kap140_ap_button", 0)
+    end
+end
+
+function AutoPilotIncreaseButtonEvent(joynum, button, downup)
+    if  currentApModeSelect == apModeSelect.Alt then
+        ipc.writeLvar("kap140_up_button", 1)
+        ipc.writeLvar("kap140_up", 1)
+        ipc.sleep(50)
+        ipc.writeLvar("kap140_up_button", 0)
+        --ipc.writeLvar("kap140_InnerKnob", 1)
+    elseif  currentApModeSelect == apModeSelect.Vs then  -- Used for Alt Arming
+        ipc.writeLvar("kap140_InnerKnob", 1)
+        -- ipc.writeLvar("kap140_up_button", 1)
+        -- ipc.writeLvar("kap140_up", 1)
+        -- ipc.sleep(50)
+        -- ipc.writeLvar("kap140_up_button", 0)
+    elseif  currentApModeSelect == apModeSelect.Hdg then
+        ipc.control(p3dControlIds.HeadingBugInc)
+    elseif  currentApModeSelect == apModeSelect.Crs then
+        ipc.control(p3dControlIds.Vor1ObiInc)
+    elseif currentApModeSelect == apModeSelect.Ias then -- Used for Baro
+        ipc.writeLvar("kap140_InnerKnob", 1)
+    end
+end
+
+function AutoPilotDecreaseButtonEvent(joynum, button, downup)
+    if  currentApModeSelect == apModeSelect.Alt then
+        ipc.writeLvar("kap140_dn_button", 1)
+        ipc.writeLvar("kap140_dn", 1)
+        ipc.sleep(50)
+        ipc.writeLvar("kap140_dn_button", 0)
+        --ipc.writeLvar("kap140_InnerKnob", -1)
+    elseif  currentApModeSelect == apModeSelect.Vs then  -- Used for Alt Arming
+        ipc.writeLvar("kap140_InnerKnob", -1)
+        -- ipc.writeLvar("kap140_dn_button", 1)
+        -- ipc.writeLvar("kap140_dn", 1)
+        -- ipc.sleep(50)
+        -- ipc.writeLvar("kap140_dn_button", 0)
+    elseif  currentApModeSelect == apModeSelect.Hdg then
+        ipc.control(p3dControlIds.HeadingBugDec)
+    elseif  currentApModeSelect == apModeSelect.Crs then
+        ipc.control(p3dControlIds.Vor1ObiDec)
+    elseif currentApModeSelect == apModeSelect.Ias then -- Used for Baro
+        ipc.writeLvar("kap140_InnerKnob", -1)
+    end
+end
+
+function AutoPilotSelectIasButtonEvent(joynum, button, downup)
+    currentApModeSelect = apModeSelect.Ias
+end
+
+function AutoPilotSelectCrsButtonEvent(joynum, button, downup)
+    currentApModeSelect = apModeSelect.Crs
+end
+
+function AutoPilotSelectHdgButtonEvent(joynum, button, downup)
+    currentApModeSelect = apModeSelect.Hdg
+end
+
+function AutoPilotSelectVsButtonEvent(joynum, button, downup)
+    currentApModeSelect = apModeSelect.Vs
+end
+
+function AutoPilotSelectAltButtonEvent(joynum, button, downup)
+    currentApModeSelect = apModeSelect.Alt
+end
+
 -- Initialise previous state
 local previousA2aEventsState = CopyA2aEventsState(currentA2aEventsState)
 
@@ -253,7 +397,6 @@ function Poll(time)
     end
 end
 
-
 --
 -- Initialise program
 --
@@ -269,11 +412,6 @@ end
 local lVarPollInterval = 250 -- Milliseconds
 event.offset(offsets.AircraftName, "STR", 0, "AircraftNameEvent")
 event.offset(offsets.AutoPilotMasterSwitch, "UD", 0, "AutoPilotMasterSwitchEvent")
-event.offset(offsets.AutoPilotNav1Lock, "UD", 0, "AutoPilotNav1LockEvent")
-event.offset(offsets.AutoPilotHeadingLock, "UD", 0, "AutoPilotHeadingLockEvent")
-event.offset(offsets.AutoPilotAltitudeLock, "UD", 0, "AutoPilotAltitudeLockEvent")
-event.offset(offsets.AutoPilotApproachHold, "UD", 0, "AutoPilotApproachHoldEvent")
-event.offset(offsets.AutoPilotBackCourseHold, "UD", 0, "AutoPilotBackCourseHoldEvent")
 
 event.Lvar("OilPressLight", lVarPollInterval, "LowOilPressureLightEvent")
 event.Lvar("FuelLight", lVarPollInterval, "LowFuelLightEvent")
@@ -290,6 +428,22 @@ event.Lvar("FSXBusVoltage", lVarPollInterval, "FSXBusVoltageEvent")
 event.Lvar("Eng1_OilTemp", lVarPollInterval, "Eng1OilTempEvent")
 event.Lvar("Eng1_OilPressure", lVarPollInterval, "Eng1OilPressureEvent")
 event.Lvar("Eng1_CHT", lVarPollInterval, "Eng1ChtEvent")
+
+event.button(0, buttons.AutoPilotHdg, 3, "AutoPilotHdgButtonEvent")
+event.button(0, buttons.AutoPilotNav, 3, "AutoPilotNavButtonEvent")
+event.button(0, buttons.AutoPilotApr, 3, "AutoPilotAprButtonEvent")
+event.button(0, buttons.AutoPilotRev, 3, "AutoPilotRevButtonEvent")
+event.button(0, buttons.AutoPilotAlt, 3, "AutoPilotAltButtonEvent")
+event.button(0, buttons.AutoPilotVs, 3, "AutoPilotVsButtonEvent")
+event.button(0, buttons.AutoPilotIas, 3, "AutoPilotIasButtonEvent")
+event.button(0, buttons.AutoPilotMaster, 3, "AutoPilotMasterButtonEvent")
+event.button(0, buttons.AutoPilotIncrease, 1, "AutoPilotIncreaseButtonEvent")
+event.button(0, buttons.AutoPilotDecrease, 1, "AutoPilotDecreaseButtonEvent")
+event.button(0, buttons.AutoPilotSelectIas, 1, "AutoPilotSelectIasButtonEvent")
+event.button(0, buttons.AutoPilotSelectCrs, 1, "AutoPilotSelectCrsButtonEvent")
+event.button(0, buttons.AutoPilotSelectHdg, 1, "AutoPilotSelectHdgButtonEvent")
+event.button(0, buttons.AutoPilotSelectVs, 1, "AutoPilotSelectVsButtonEvent")
+event.button(0, buttons.AutoPilotSelectAlt, 1, "AutoPilotSelectAltButtonEvent")
 
 -- Initialse all LEDs by turning them off
 local commandString = bravoLedBitsProcessor.GetFeatureStringFromLedBits()
